c24ec07445f4bcc9a3294228fc6e11ea2183a47c
Nic Aguiluz <nicolasaguiluz2@gmail.com>
2021-03-16 20:23:12 -0500

    Update pokemon quiz.py

--- pokemon quiz.py
+++ pokemon quiz.py
@@ -1,3 +1,5 @@
+#This is a quiz on first generation pokemon games (red/blue/yellow)
+
 class Question: #we create and class called question so that we can input several types of data as one item
     def __init__(self, prompt, answer): #we set the values of the object as prompt and answer
         self.prompt = prompt
